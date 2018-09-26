2be870c5491f887229a7e19f32f8162efcb458c7
Nicholas Westley <nicholaswestley@gmail.com>
2018-09-26 15:39:53 +0700

    better styling for recipes

--- resources/js/components/RecipeList.vue
+++ resources/js/components/RecipeList.vue
@@ -2,7 +2,11 @@
     <div class="recipe-list">
         <h4>Recipes</h4>
         <div class="recipe-list-container">
-            <div class="recipe-square" v-for="recipe in recipes">
+            <div
+                v-bind:style="{
+                    'background-image': 'linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)), url(assets/images/' + recipe.image + ')'
+                }"
+                class="recipe-square" v-for="recipe in recipes">
                 <h4>{{ recipe.name }}</h4>
                 {{ recipe.description }}
             </div>
@@ -34,9 +38,13 @@
     }
 
     .recipe-square {
-        width: 150px;
+        width: 200px;
         height: 150px;
         margin: 0 10px;
         background-color: #FEFFFE;
+        color: white;
+        text-shadow: #1b1e21;
+        padding: 8px;
+        cursor: pointer;
     }
 </style>
