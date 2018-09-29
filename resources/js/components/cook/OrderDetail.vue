5edfbd2393e7ef0129ffd6fff37f1632620f974d
Nicholas Westley <nicholaswestley@gmail.com>
2018-09-29 12:00:35 +0700

    split order detail item from order detail

--- resources/js/components/cook/OrderDetail.vue
+++ resources/js/components/cook/OrderDetail.vue
@@ -8,75 +8,25 @@
             style="margin-bottom: 16px;"
         />
 
-
-
-        <div
-            class="order-item"
+        <order-detail-order-item
             v-for="item in selectedOrder.order_items"
-            :class="{
-                'item-completed': item.completed,
-                'item-in-progress': item.in_progress
-             }">
-            <!-- ORDER STATE INFO -->
-            <span v-if="item.completed">Item Completed</span>
-            <!-- GENERAL ORDER ITEM INFO -->
-            <h4>{{ item.recipe.name }}</h4>
-            <div> {{ item.recipe.description }} </div>
-            <!-- ORDER OPERATIONS -->
-            <order-item-operations
-                :item="item"
-                @markItemInProgress="$emit('markItemInProgress', item)"
-                @markItemCompleted="$emit('markItemCompleted', item)"/>
-            <!-- INGREDIENTS -->
-            <div v-for="ingredient in item.recipe.ingredients">
-                <span :class="selectedIngredients[item.id][ingredient.id] ? 'selected' : 'not-selected'">
-                    {{ ingredient.name }}
-                </span>
-            </div>
-        </div>
+            :key="item.id"
+            :item="item"
+            :selectedOrder="selectedOrder"
+            @markItemInProgress="$emit('markItemInProgress', item)"
+            @markItemCompleted="$emit('markItemCompleted', item)"/>
     </div>
 </template>
 
 <script>
     import OrderOperations from './OrderOperations';
-    import OrderItemOperations from './OrderItemOperations';
+    import OrderDetailOrderItem from './OrderDetailOrderItem';
     export default {
         name: "order-detail",
         props: ['selectedOrder'],
-        components: { OrderOperations, OrderItemOperations },
-        computed: {
-            selectedIngredients() {
-                const selected = {};
-                this.selectedOrder.order_items.forEach(item => {
-                    selected[item.id] = {};
-                    item.order_item_ingredient_maps.forEach(map =>{
-                        selected[item.id][map.ingredient_id] = true;
-                    });
-                });
-                return selected;
-            }
+        components: {
+            OrderOperations,
+            OrderDetailOrderItem
         }
     }
 </script>
-
-<style scoped>
-    .order-item {
-        border-top: 1px solid #b8c2cc;
-        padding: 8px;
-
-    }
-    .selected {
-        font-weight: bold;
-    }
-    .not-selected {
-        color: #b8c2cc;
-    }
-    .item-completed {
-        opacity: 0.3;
-    }
-    .item-in-progress {
-        background-color: green;
-        color: white;
-    }
-
-</style>
---
+++ resources/js/components/cook/OrderDetailOrderItem.vue
@@ -0,0 +1,61 @@
+<template>
+    <div
+        class="order-item"
+        :class="{
+                'item-completed': item.completed,
+                'item-in-progress': item.in_progress
+             }">
+        <!-- ORDER STATE INFO -->
+        <span v-if="item.completed">Item Completed</span>
+        <!-- GENERAL ORDER ITEM INFO -->
+        <h4>{{ item.recipe.name }}</h4>
+        <div> {{ item.recipe.description }} </div>
+        <!-- ORDER OPERATIONS -->
+        <order-item-operations
+            :item="item"
+            @markItemInProgress="$emit('markItemInProgress')"
+            @markItemCompleted="$emit('markItemCompleted')"/>
+        <!-- INGREDIENTS -->
+        <div v-for="ingredient in item.recipe.ingredients">
+                <span :class="selectedIngredients[item.id][ingredient.id] ? 'selected' : 'not-selected'">
+                    {{ ingredient.name }}
+                </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    import OrderItemOperations from './OrderItemOperations';
+    export default {
+        name: "order-detail-order-item",
+        props: ['item', 'selectedOrder'],
+        components: { OrderItemOperations },
+        computed: {
+            selectedIngredients() {
+                const selected = {};
+                this.selectedOrder.order_items.forEach(item => {
+                    selected[item.id] = {};
+                    item.order_item_ingredient_maps.forEach(map =>{
+                        selected[item.id][map.ingredient_id] = true;
+                    });
+                });
+                return selected;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .order-item {
+        border-top: 1px solid #b8c2cc;
+        padding: 8px;
+
+    }
+    .item-completed {
+        opacity: 0.3;
+    }
+    .item-in-progress {
+        background-color: green;
+        color: white;
+    }
+</style>
