41504601983a5f0d25172a402abd9555ed1ed28d
Nicholas Westley <nicholaswestley@gmail.com>
2018-09-30 16:29:21 +0700

    add username to register form

--- app/User.php
+++ app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-    protected $fillable = [ 'name', 'email', 'password'];
+    protected $fillable = [ 'name', 'email', 'password', 'username'];
     protected $hidden = ['password', 'remember_token'];
 
     /* RELATIONSHIP */
--- resources/views/webapp.blade.php
+++ resources/views/webapp.blade.php
@@ -3,5 +3,4 @@
 @section('content')
     <div id="vue"></div>
 @endsection
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}" defer></script>
+
