d9a4ec1f028397cf9afb2e2572544ce8283d3d24
Nicholas Westley <nicholaswestley@gmail.com>
2018-10-11 14:19:29 +0700

    delete renamed files

--- resources/views/inc/dashboard/accept-invitation.blade.php
+++
@@ -1,6 +0,0 @@
-{!!Form::open([
-    'action' => ['InvitationsController@accept', $invitation['restaurant']['id'], $invitation->id],
-    'method' => 'POST',
-    'style' => 'display: inline-block;']) !!}
-{{ Form::bsSubmit('Accept', ['class'=>'btn btn-success btn-sm']) }}
-{{ Form::close() }}
--- resources/views/inc/dashboard/delete-restaurant.blade.php
+++
@@ -1,8 +0,0 @@
-{!!Form::open([
-    'action' => ['DashboardController@destroy', $restaurant->id],
-    'onsubmit' => 'return confirm("Are you sure")',
-    'method' => 'POST',
-    'style' => 'display: inline-block;']) !!}
-{{ Form::hidden('_method', 'DELETE') }}
-{{ Form::bsSubmit('Delete', ['class'=>'btn btn-danger btn-sm']) }}
-{{ Form::close() }}
