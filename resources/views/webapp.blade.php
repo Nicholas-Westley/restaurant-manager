e651b5c844632002bdbe6f25812d615543afb2fa
Nicholas Westley <nicholaswestley@gmail.com>
2018-10-12 09:11:49 +0700

    don't need all the admin options displaying on the web app

--- resources/views/webapp.blade.php
+++ resources/views/webapp.blade.php
@@ -1,6 +1,26 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Food Order Tracker') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
     <script>
         window.restaurant = {{ $restaurant_id }}
         window.user_settings = {!! json_encode($settings) !!}
@@ -8,5 +28,7 @@
     <div id="vue">
 
     </div>
-@endsection
+</div>
+</body>
+</html>
 
