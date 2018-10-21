137812b3390ac90d8875817694023d316b47ce51
Nicholas Westley <nicholaswestley@gmail.com>
2018-10-21 20:53:49 +0700

    cast Ingredient.optional & Ingredient.selected_by_default to boolean

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Ingredient extends Model {
 
     protected $fillable = ['name', 'recipe_id', 'optional', 'selected_by_default'];
+    protected $casts = [
+        'optional' => 'boolean',
+        'selected_by_default' => 'boolean'
+    ];
+
     /* RELATIONSHIP */
     public function recipe() {
         return $this->belongsTo('App\Recipe');
