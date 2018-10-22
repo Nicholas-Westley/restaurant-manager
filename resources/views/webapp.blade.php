fe49ebb387a0d593f5af14e9667bdf43ebb8c21b
Nicholas Westley <nicholaswestley@gmail.com>
2018-10-22 10:57:21 +0700

    Change app name

--- resources/views/webapp.blade.php
+++ resources/views/webapp.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Food Order Tracker') }}</title>
+    <title>{{ config('app.name', 'Restaurant Manager') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
